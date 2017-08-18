7e5d9df90ed3d87b3562cecce6a0f86f3e7bdadc
231344781@qq.com <231344781@qq.com>
2017-08-19 00:56:51 +0800

    add

---
+++ FXB/DataManager/QtMgr.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FXB.DataManager
+{
+    class QtMgr
+    {
+        private static QtMgr ins;
+
+        private QtMgr()
+        {
+
+        }
+
+        public static QtMgr Instance()
+        {
+            if (ins == null)
+            {
+                ins = new QtMgr();
+            }
+
+            return ins;
+        }
+
+
+    }
+}
